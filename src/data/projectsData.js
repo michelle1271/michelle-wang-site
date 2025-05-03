52af79d03b8ce00a592f4794e6976227e8412a70
michelle1271 <michellewangnini@icloud.com>
2025-05-03 19:38:09 -0400

    Updated Projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,31 +6,37 @@ import Bloom from '../assets/bloom.png';
 import StampExplore from '../assets/stamp-explore.png';
 import Anova from '../assets/anova.png';
 import Solidworks from '../assets/solidworks.png';
-import PDS from '../assets/bloom-pds-2025.pdf';
 import Stamp from '../assets/stamp-explore.pdf';
-import Mosaic from '../assets/anova-mosaic-2025.pdf';
-
-
 
 const projectsData = [
     {
       id: 1,
-      link: PDS,
+      link: "https://ultra-ninja-9b8.notion.site/Bloom-1e5f8000022080eb8114e59bb0b9cde7?pvs=4",
       title: "Bloom",
       description: "A social platform designed to help seniors combat loneliness. Prioritized accessibility, safety, and ease of use to empower seniors to stay connected, engaged, and socially active in their communities. Presented at the Western Product Design Spring and awarded Best Beginner Design.",
       image: Bloom,
       tags: ["Figma", "Product Thinking", "Visual Design"],
     },
+
     {
       id: 2,
+      link: "https://ultra-ninja-9b8.notion.site/Anova-1e7f800002208005a24aee0b3ca151d6?pvs=4",
+      title: "Anova",
+      description: "An all-in-one workplace application designed to enhance communication, coordination, and consistency for hybrid and remote teams. Key features to address concerns of hybrid work, difficult onboarding, and communication. Presented at Waterloo’s GBDA design-a-thon Mosaic.",
+      image: Anova,
+      tags: ["Figma", "Product Thinking", "Visual Design"],
+    },
+
+    {
+      id: 3,
       link: Stamp,
       title: "StampExplore",
-      description: "Learned several aspects of web development through online videos, code-alongs, and mini projects, from HTML/CSS to JavaScript and React. Taught the importance of responsive designs and the use of framework tools such as Bulma.",
+      description: "StampExplore is a mobile app concept that lets users collect virtual stamps for each city they visit, creating a digital alternative to traditional souvenirs. Each stamp links to a customizable photo album with trip details, allowing users to visually document and revisit their travels.",
       image: StampExplore,
       tags: ["Figma", "Product Thinking", "Visual Design"],
     },
     {
-      id: 3,
+      id: 4,
       link: "https://github.com/michelle1271/Udemy-Web-Developer-Bootcamp-Coursework.git",
       title: "Udemy: The Web Developer Bootcamp",
       description: "Covered the fundamentals of web development through online videos and code-alongs, from HTML/CSS to JavaScript and React. Taught the importance of responsive designs and the use of framework tools such as Bulma.",
@@ -38,24 +44,17 @@ const projectsData = [
       tags: ["HTML/CSS", "JavaScript", "React", "Web Development"],
     },
     {
-      id: 4,
-      link: "",
+      id: 5,
+      link: "https://www.canva.com/design/DAGPABePpTA/RFmuRpGn0cg0NDGWeidxwQ/view?utm_content=DAGPABePpTA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb1af3fd9f0",
       title: "The Mystery Machine 3D Model",
       description: "Solidworks 3D model of Scooby Doo's Mystery Machine. Practicing 3D modeling and moving components (ex. functional doors made with hinges, steering wheel, wheels and axels etc).",
       image: Solidworks,
       tags: ["SolidWorks", "3D Modeling", "CAD"],
     },
-    {
-      id: 5,
-      link: Mosaic,
-      title: "Anova",
-      description: "An all-in-one workplace application designed to enhance communication, coordination, and consistency for hybrid and remote teams. Key features to address concerns of hybrid work, difficult onboarding, and communication. Presented at Waterloo’s GBDA design-a-thon Mosaic.",
-      image: Anova,
-      tags: ["Figma", "Product Thinking", "Visual Design"],
-    },
+  
     {
       id: 6,
-      link: "",
+      link: "https://www.canva.com/design/DAGPABePpTA/RFmuRpGn0cg0NDGWeidxwQ/view?utm_content=DAGPABePpTA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb1af3fd9f0",
       title: "Flappy Bird",
       description: "A space-themed model of FlappyBird. Made with interactive components to control the movement of FlappyBird, obstacles, and backdrops. Created and designed all the pixelated objects and backdrop. ",
       image: FlappyBird,
@@ -63,7 +62,7 @@ const projectsData = [
     },
     {
       id: 7,
-      link: "",
+      link: "https://www.canva.com/design/DAGPABePpTA/RFmuRpGn0cg0NDGWeidxwQ/view?utm_content=DAGPABePpTA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb1af3fd9f0",
       title: "Wardrobe Visualizer",
       description: "As someone who prefers to online shop but wants to try on the clothes to see what it would look like, the Wardrobe Visualizer helps me 'try on' the clothes, and mix & match it with other articles of clothing either from my closet, or other online products.",
       image: Wardrobe,
--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
   return (
     <div className='projects'>
       <h1>projects</h1>
-      <a className="portfolio" href="https://www.canva.com/design/DAGPABePpTA/RFmuRpGn0cg0NDGWeidxwQ/view?utm_content=DAGPABePpTA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb1af3fd9f0" target="_blank" rel="noopener noreferrer"><h2>view my complete portfolio</h2></a>
+      <a className="portfolio" href="https://www.canva.com/design/DAGPABePpTA/RFmuRpGn0cg0NDGWeidxwQ/view?utm_content=DAGPABePpTA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb1af3fd9f0" target="_blank" rel="noopener noreferrer"><h2>click each panel or view my complete portfolio here</h2></a>
       <div className="projects-page">
       <div className="projects-grid">
         {projectsData.map((projectData) => (
